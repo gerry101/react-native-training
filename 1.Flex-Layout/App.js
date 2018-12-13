918cdcf7d2ec816de5f93ddea7caa86c83bbbd6a
Gerry <gerrymigwi6@gmail.com>
2018-12-13 03:25:16 +0200

    1-2-stylesheet

--- 1.Flex-Layout/App.js
+++ 1.Flex-Layout/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{'flex': 1, 'alignItems':'center', 'justifyContent': 'center', 'backgroundColor': 'orange'}}>
-        <Text style={{'color': 'white'}}>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Just changed the text!</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {'flex': 1, 'alignItems':'center', 'justifyContent': 'center', 'backgroundColor': 'orange',},
+  text: {'color': 'white', 'fontSize': 23},
+});
